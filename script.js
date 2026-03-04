53c515f13f2aa1c9c78eb472d1f7948ae02540af
robert_debauch <robert.debauch@gmail.com>
2026-03-04 12:03:04 +0500

    feat: add style AND create DOM-structure to implement

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const myLibrary = [];
 
-function Book(id, title, author, genre, pages, read) {
+function Book(id, genre, title, author, pages, read) {
     if (!new.target) {
         throw Error("You must use the new operator to call the constructor");
     }
 
     this.id = id;
+    this.genre = genre;
     this.title = title;
     this.author = author;
-    this.genre = genre;
     this.pages = pages;
     this.read = read;
 }
@@ -16,17 +16,103 @@ function Book(id, title, author, genre, pages, read) {
 let id = crypto.randomUUID();
 
 
-function addBookToLibrary(id, title, author, genre, pages, read) {
-    const book = new Book(id, title, author, genre, pages, read);
+function addBookToLibrary(id, genre, title, author, pages, read) {
+    const book = new Book(id, genre, title, author, pages, read);
     myLibrary.push(book);
 }
 
-addBookToLibrary(crypto.randomUUID(), 'test', 'test', 'cookbook', 200, true);
-addBookToLibrary(crypto.randomUUID(), 'test', 'test', 'cookbook', 200, false);
-addBookToLibrary(crypto.randomUUID(), 'test', 'test', 'cookbook', 400, false);
+addBookToLibrary(crypto.randomUUID(), 'cookbook', 'test', 'test', 200, true);
+addBookToLibrary(crypto.randomUUID(), 'cookbook', 'test', 'test', 200, false);
+addBookToLibrary(crypto.randomUUID(), 'cookbook', 'test', 'test', 400, false);
 
-console.log(myLibrary[0]);
-console.log(myLibrary[1]);
-console.log(myLibrary[2]);
+/* elements in DOM */
+
+function createBook() {
+const bookShelf = document.querySelector(".bookshelf");
+console.log(bookShelf);
+
+const bookEntry = document.createElement('div');
+bookEntry.classList.add('book-entry');
+console.log(bookEntry);
+
+bookShelf.append(bookEntry);
+
+const readStatus = document.createElement('div');
+readStatus.classList.add('readStatus');
+readStatus.textContent = 'not read yet'; /*temp*/
+bookEntry.append(readStatus);
+
+const frame = document.createElement('div');
+frame.classList.add('frame');
+const picture = document.createElement('div');
+picture.classList.add('picture');
+
+frame.append(picture);
+bookEntry.append(frame);
+
+const bookInfo = document.createElement('div');
+bookInfo.classList.add('bookinfo');
+bookEntry.append(bookInfo);
+
+const primaryInfo = document.createElement('div');
+primaryInfo.classList.add('primary-info');
+
+const secondaryInfo = document.createElement('div');
+secondaryInfo.classList.add('secondary-info');
+
+bookInfo.append(primaryInfo);
+bookInfo.insertBefore(secondaryInfo, primaryInfo);
 
 
+const genre = document.createElement('div');
+const genreText = document.createTextNode('genre info'); /*temp*/
+genre.append(genreText);
+
+const pages = document.createElement('div');
+const numberOfPages = document.createTextNode('pages: 50'); /*temp. num? */
+pages.append(numberOfPages);
+
+const title = document.createElement('h3');
+const titleText = document.createTextNode('Poetry Seeing #2');
+title.append(titleText);
+
+const author = document.createElement('p');
+const authorText = document.createTextNode('Takahiro Kurashima');
+author.append(authorText);
+
+genre.classList.add('book', 'genre');
+pages.classList.add('book', 'pages');
+title.classList.add('book', 'title');
+author.classList.add('book', 'author');
+
+primaryInfo.append(title);
+primaryInfo.append(author);
+secondaryInfo.append(genre);
+secondaryInfo.append(pages);
+};
+
+
+
+
+function showBooks(array) {
+    array.forEach((book) => {
+        
+
+        createBook(); /* Only for test: i can see that the number of objects in array have a relationship with number of books that was created by temprorary function */
+        const info = function () {
+            let readStatus;
+
+            if (book.read === true) {
+                readStatus = 'completed';
+            } else {
+                readStatus = 'not read yet';
+            }
+
+            console.log(` genre: ${book.genre}, title: ${book.title}, author: ${book.author}, pages: ${book.pages}, read: ${readStatus}`)
+        }
+        /*id: ${book.id} - i don't think it's good to display the id*/
+        info(book);
+    });
+}
+
+showBooks(myLibrary);
